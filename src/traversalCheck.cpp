717bd57484238a435004024f2b040d698599c3e7
yh <zjuyh@sina.cn>
2018-04-07 21:10:17 +0800

    boundary line coarse

--- src/traversalCheck.cpp
+++ src/traversalCheck.cpp
@@ -43,6 +43,7 @@ public:
     double stepRadiusFirst;
     double stepRadiusSecond;
     int cellsCritical;
+    int boundCritical;
 
     void check(const grid_map_msgs::GridMap& gridMapIn);
 
@@ -60,7 +61,8 @@ Traversability::Traversability(ros::NodeHandle& n):
     stepCritical(getParam<double>("stepCritical", 0)),
     stepRadiusFirst(getParam<double>("stepRadiusFirst", 0)),
     stepRadiusSecond(getParam<double>("stepRadiusSecond", 0)),
-    cellsCritical(getParam<int>("cellsCritical", 0))
+    cellsCritical(getParam<int>("cellsCritical", 0)),
+    boundCritical(getParam<int>("boundCritical", 0))
 {
     gridPublisher = n.advertise<grid_map_msgs::GridMap>("grid_map_travelChecked", 1, true);
     occuMapPublisher = n.advertise<nav_msgs::OccupancyGrid>("occuMap", 1, true);
@@ -79,6 +81,7 @@ void Traversability::check(const grid_map_msgs::GridMap& gridMapIn)
     /// from ethz_asl step_filter of traversability estimation
     localGridMap.add("traversability_step");
     localGridMap.add("step_height");
+    localGridMap.add("boundary_line");
     double height, step;
 
     // First iteration through the elevation map.
@@ -126,11 +129,14 @@ void Traversability::check(const grid_map_msgs::GridMap& gridMapIn)
       double stepMax = 0.0;
       bool isValid = false;
 
+      int flatCount = 0;
+      int hillCount = 0;
+
       // Requested position (center) of circle in map.
       Eigen::Vector2d center;
       localGridMap.getPosition(*iterator, center);
 
-      // Compute the step height.
+      // Compute the traversability and the boundary, according to the step_height.
       for (CircleIterator submapIterator(localGridMap, center, stepRadiusSecond);
           !submapIterator.isPastEnd(); ++submapIterator)
       {
@@ -143,7 +149,16 @@ void Traversability::check(const grid_map_msgs::GridMap& gridMapIn)
         }
         if (localGridMap.at("step_height", *submapIterator) > stepCritical)
           nCells++;
+
+        // can be noted
+        localGridMap.at("step_height", *submapIterator)>0?hillCount++:flatCount++;
       }
+
+      // can be noted, for boundary line
+      if(flatCount > 0 && hillCount > 0 && abs(flatCount-hillCount) <= boundCritical)
+            localGridMap.at("boundary_line", *iterator) = 1;
+      else
+            localGridMap.at("boundary_line", *iterator) = 0;
 
       if (isValid)
       {
